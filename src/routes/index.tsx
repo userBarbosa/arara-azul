38011edb17de3be71eb9142022f7921030dd0d42
anabalves <ana.alves41@fatec.sp.gov.br>
2022-09-14 16:56:58 -0300

    feat: adicionado mui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Link, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
     return (
       <Routes>
-        <Route path="/" element={<p>Teste</p>} />
+        <Route path="/" element={<Button>Teste</Button>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     );

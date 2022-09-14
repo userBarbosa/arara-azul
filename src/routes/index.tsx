fffcafeb67fdcb4ca666c4e789b96de888ee6983
anabalves <ana.alves41@fatec.sp.gov.br>
2022-09-14 16:46:02 -0300

    feat: adicionado react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export { };
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+    return (
+      <Routes>
+        <Route path="/" element={<p>Teste</p>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    );
+}

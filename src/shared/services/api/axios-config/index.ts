393ff22c7bd969fa9d1ca5246caaded783d61f90
anabalves <ana.alves41@fatec.sp.gov.br>
2022-10-17 11:18:48 -0300

    feat: adicionado configuracoes de ambiente

--- src/shared/services/api/axios-config/index.ts
+++ src/shared/services/api/axios-config/index.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
-import { errorInterceptor, responseInterceptor } from './interceptors';
+
+import { responseInterceptor, errorInterceptor } from './interceptors';
+import { Environment } from '../../../environment';
+
 
 const Api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: Environment.BASE_URL
 });
 
 Api.interceptors.response.use(

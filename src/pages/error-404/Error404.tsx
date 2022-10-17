bc2205d2fe2240673ede70209548479bf68a4c8f
anabalves <ana.alves41@fatec.sp.gov.br>
2022-10-17 11:16:27 -0300

    feat: criado base para pagina de erro 404

--- src/pages/error-404/Error404.tsx
+++ src/pages/error-404/Error404.tsx
@@ -1,5 +1,30 @@
-import { Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { LayoutPageError } from '../../shared/layouts';
+import { ReactComponent as ImageError404 } from '../../assets/error404.svg';
+import { Theme, useMediaQuery, useTheme } from '@mui/material';
 
 export const Error404: React.FC = () => {
-  return (<Box>Erro 404</Box>);
+  const down350 = useMediaQuery((theme: Theme) => theme.breakpoints.down(350));
+  const mddown = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
+  const mdUp = useMediaQuery((theme: Theme) => theme.breakpoints.up('md'));
+  const lgUp = useMediaQuery((theme: Theme) => theme.breakpoints.up('lg'));
+
+  const theme = useTheme();
+  
+  const navigate = useNavigate();
+
+  const navigateHome = () => {
+    navigate('/home');
+  };
+
+  return (
+    <LayoutPageError 
+      image={<ImageError404 
+        height={theme.spacing(down350 ? 30 : mddown ? 40 : mdUp ? 70 : lgUp ? 80 : 100)} 
+        width={theme.spacing(down350 ? 30 : mddown ? 40 : mdUp ? 70 : lgUp ? 80 : 100)}/>}
+      showButton 
+      buttonText='Home'
+      onClickButton={navigateHome}
+    ></LayoutPageError>
+  );
 };
